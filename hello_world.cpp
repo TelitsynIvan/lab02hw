26b3ecdef9311dde59f7dc827eb20663690ba42f
TelitsynIvan <neleonzend@gmail.com>
2022-03-21 17:09:22 +0300

    Mozilla format

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,11 +3,11 @@
 #include <iostream>
 #include <string>
 
-
-int main(int argc, char* argv){
-	std::string name;
-	std::cout << "Input user_name":
-	std::cin >> name;
-	std::cout << "Hello world from " << name << endl;
-	return 0;
+int
+main(int argc, char* argv)
+{
+  std::string name;
+  std::cout << "Input user_name" : std::cin >> name;
+  std::cout << "Hello world from " << name << endl;
+  return 0;
 }
